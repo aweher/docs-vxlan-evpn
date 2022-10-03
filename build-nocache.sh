4a01571de37fd316a56f884e98f27aa7839bea7f
Ariel S. Weher <ariel@weher.net>
2022-10-02 23:00:45 -0400

    added built type without cache

--- build-nocache.sh
+++ build-nocache.sh
@@ -2,6 +2,6 @@
 # ASW
 PROYECTO="reg.dc.celp.red/lacnog/doc-lab-vxlan-evpn"
 NOW=$(date +%Y%m%d%H%M%S)
-docker build -t $PROYECTO:$NOW -t $PROYECTO:latest .
+docker build --no-cache -t $PROYECTO:$NOW -t $PROYECTO:latest .
 docker push $PROYECTO:$NOW
 docker push $PROYECTO:latest
